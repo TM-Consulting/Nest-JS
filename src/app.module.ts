93973ba1de15e45ee85345f971387e2d7b5db10d
zakaria-ho-ss <78755613+zakaria-ho-ss@users.noreply.github.com>
2021-05-31 19:59:36 +0100

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config';
-import { CarsModule } from './modules/cars/cars.module';
 import { UserModule } from './modules/user/user.module';
+console.log(config.database.mongoDB_Cluster)
 @Module({
   imports: [
     UserModule,
-    CarsModule,
     MongooseModule.forRoot(config.database.mongoDB_Cluster),
   ],
   controllers: [AppController],

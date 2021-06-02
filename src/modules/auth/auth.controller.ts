4b736fa92569c60384af56b439e6184a2a8505c5
TM-Consulting <mounaim.touinssi@gmail.com>
2021-06-02 19:40:34 +0200

    auth

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+}

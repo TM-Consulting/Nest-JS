918525c77d4880c4c2f57ce65a37c3b21ef210d4
TM-Consulting <mounaim.touinssi@gmail.com>
2021-06-05 14:39:06 +0200

    add salt

--- src/modules/user/user.models.ts
+++ src/modules/user/user.models.ts
@@ -2,7 +2,7 @@ import { Length, IsEmail, IsOptional } from 'class-validator';
 import * as mongoose from 'mongoose';
 
 export class userDTO {
-  @Length(6, 30)
+  @Length(2, 30)
   name: string;
 
   @Length(6, 40)
@@ -14,7 +14,7 @@ export class userDTO {
 }
 
 export class updateUserDTO {
-  @Length(6, 30)
+  @Length(2, 30)
   @IsOptional()
   name?: string;
 
@@ -24,10 +24,20 @@ export class updateUserDTO {
   email?: string;
 }
 
+export class loginDTO {
+  @Length(6, 30)
+  password: string;
+
+  @Length(6, 40)
+  @IsEmail()
+  email: string;
+}
+
 export const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  salt: { type: String, required: true },
   email_verified: { type: Boolean, required: false, default: false },
 });
 
@@ -36,5 +46,6 @@ export interface User extends mongoose.Document {
   name: string;
   email: string;
   password: string;
+  salt: string;
   email_verified: boolean;
 }

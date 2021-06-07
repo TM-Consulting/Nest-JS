5b0ae4b859557922bbd902c647954824f2a5077e
BOUALI ABDELLATIF <abdellatif.bouali@gmail.com>
2021-06-07 20:59:37 +0000

    PUSH3

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,42 +4,41 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  login(userDTO: loginDTO) {
-    login = async (loginDto: loginDTO) => {
-      const user = await this.findOne(loginDto.email);
-      if (user) {
-        console.log(
-          'testt ',
-          user.password,
-          await bcrypt.hash(loginDto.password, user.salt),
-        );
-        if ((await bcrypt.hash(loginDto.password, user.salt)) === user.password) {
-          return {
-            operation: {
-              success: true,
-              message: 'login successfully',
-              data: { user: user },
-            },
-          };
-        } else {
-          return {
-            operation: {
-              success: false,
-              message: 'login failed',
-              data: { user: null },
-            },
-          };
-        }
+   login = async (loginDto: loginDTO) => {
+    const user = await this.findOne(loginDto.email);
+    if (user) {
+      console.log(
+        'testt ',
+        user.password,
+        await bcrypt.hash(loginDto.password, user.salt),
+      );
+      if ((await bcrypt.hash(loginDto.password, user.salt)) === user.password) {
+        return {
+          operation: {
+            success: true,
+            message: 'login successfully',
+            data: { user: user },
+          },
+        };
       } else {
         return {
           operation: {
             success: false,
-            message: 'user not exist',
+            message: 'login failed',
             data: { user: null },
           },
         };
       }
-    };
+    } else {
+      return {
+        operation: {
+          success: false,
+          message: 'user not exist',
+          data: { user: null },
+        },
+      };
+    }
+  };
   }
 
   

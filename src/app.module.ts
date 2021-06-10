e4b21f65f1ab14851f9c7defb061ee59200c96ac
TM-Consulting <mounaim.touinssi@gmail.com>
2021-06-10 21:03:28 +0200

    send email

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import config from './config';
 import { AuthModule } from './modules/auth/auth.module';
 import { CarsModule } from './modules/cars/cars.module';
+import { MailModule } from './modules/mail/mail.module';
 import { UserModule } from './modules/user/user.module';
 console.log(config.database.mongoDB_Cluster);
 @Module({
   imports: [
     AuthModule,
     UserModule,
+    MailModule,
     CarsModule,
     MongooseModule.forRoot(config.database.mongoDB_Cluster),
   ],

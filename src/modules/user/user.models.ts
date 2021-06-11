d5c8faa66223bdb1bea0f2348d047f60f608f5d1
TM-Consulting <mounaim.touinssi@gmail.com>
2021-06-11 21:17:05 +0200

    add doc

--- src/modules/user/user.models.ts
+++ src/modules/user/user.models.ts
@@ -1,19 +1,30 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Length, IsEmail, IsOptional } from 'class-validator';
 import * as mongoose from 'mongoose';
 
 export class userDTO {
+  @ApiProperty({ example: 'Monaim' })
   @Length(2, 30)
   name: string;
 
+  @ApiProperty({
+    example: '123456',
+    description: ' must me an email >6 crtr <15 crtr',
+  })
   @Length(6, 40)
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: '123456',
+    description: ' must me > 6 crts <15 crts ',
+  })
   @Length(6, 15)
   password: string;
 }
 
 export class updateUserDTO {
+  @ApiPropertyOptional({ example: 'monaim' })
   @Length(2, 30)
   @IsOptional()
   name?: string;
@@ -25,9 +36,10 @@ export class updateUserDTO {
 }
 
 export class loginDTO {
+  @ApiProperty({ example: '123456' })
   @Length(6, 30)
   password: string;
-
+  @ApiProperty({ example: 'monaim@example.com' })
   @Length(6, 40)
   @IsEmail()
   email: string;
